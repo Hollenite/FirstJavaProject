26ed162430f68a11906f53086c022de0a00bba78
ariyan <ariyanpradhan112@gmail.com>
2025-06-29 17:00:08 +0530

    commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+    public static void main(String [] args) {
+        byte age = 30;
+        long number1 = 273923928320L;
+        float number2 = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+        Date now = new Date();
+//        System.out.println(now);
+//        String message = new String("Hello World" + "!!");
+//        System.out.println(message);
+        int[] numbers = {1, 4 ,6 ,9, 10};
+//        System.out.println(numbers.length);
+//        System.out.println(Arrays.toString(numbers));
+//        System.out.println(numbers[0] + numbers[2]);
+        float result = Math.max(1.0F, 3.2F);
+        System.out.println(result);
+        double result1 = Math.random();
+
+//        System.out.println("Hello World, my age is " + age);
+    }
+}
