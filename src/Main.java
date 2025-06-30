dcefe8488723d130e181a145ca7c2a52f9565871
ariyan <ariyanpradhan112@gmail.com>
2025-06-30 12:04:50 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,24 @@
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args) {
-        byte age = 30;
-        long number1 = 273923928320L;
-        float number2 = 10.99F;
-        char letter = 'A';
-        boolean isEligible = true;
-        Date now = new Date();
-//        System.out.println(now);
-//        String message = new String("Hello World" + "!!");
-//        System.out.println(message);
-        int[] numbers = {1, 4 ,6 ,9, 10};
-//        System.out.println(numbers.length);
-//        System.out.println(Arrays.toString(numbers));
-//        System.out.println(numbers[0] + numbers[2]);
-        float result = Math.max(1.0F, 3.2F);
-        System.out.println(result);
-        double result1 = Math.random();
-
-//        System.out.println("Hello World, my age is " + age);
+        Scanner in = new Scanner(System.in);
+                System.out.print("principal: ");
+        int Principal = in.nextInt();
+                System.out.print("Interest: ");
+        double Interest = in.nextDouble();
+                System.out.print("Period(years): ");
+        int Age = in.nextInt();
+        Interest /= 100;
+        double numerator = Principal * (Interest/12) * Math.pow(((double)1 + (Interest/12)), (12 * Age));
+        double denominator = (Math.pow((1 + (Interest/12)), (12 * Age))) - 1;
+        double final_value = (numerator/denominator);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+            String var = currency.format(final_value);
+        System.out.println(var);
+        System.out.println("$" + final_value);
     }
 }
